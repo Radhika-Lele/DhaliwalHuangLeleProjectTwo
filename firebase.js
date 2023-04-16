629e3558b89320c0552974215fc6401db6c98077
noelle7 <noellehuang07@gmail.com>
2023-04-16 18:27:07 -0400

    edited html/css styling

---
+++ firebase.js
@@ -0,0 +1,35 @@
+
+
+// Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyAC9rdgoBx9Grbr4wZHYXhBUth749_M7jE",
+
+    authDomain: "pair-project-two-2796e.firebaseapp.com",
+
+    projectId: "pair-project-two-2796e",
+
+    storageBucket: "pair-project-two-2796e.appspot.com",
+
+    messagingSenderId: "671424892462",
+
+    appId: "1:671424892462:web:90ea73dd14183588cf19bf"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
+
+  export default app;
