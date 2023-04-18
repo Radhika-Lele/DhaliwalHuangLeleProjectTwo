e09bd1bf19f198191d521bb348f3e984865a7de1
dilpreetdhaliwal <dilpreet13dhaliwal@gmail.com>
2023-04-18 18:20:06 -0400

    test

---
+++ scripts/firebase.js
@@ -0,0 +1,57 @@
+
+//   // Import the functions you need from the SDKs you need
+//   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+//   // TODO: Add SDKs for Firebase products that you want to use
+//   // https://firebase.google.com/docs/web/setup#available-libraries
+
+//   // Your web app's Firebase configuration
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyCMf7c7lSpwaXADTvPUIU0xRW4SfaA8Zhk",
+//     authDomain: "pair-project-two.firebaseapp.com",
+//     databaseURL: "https://pair-project-two-default-rtdb.firebaseio.com",
+//     projectId: "pair-project-two",
+//     storageBucket: "pair-project-two.appspot.com",
+//     messagingSenderId: "304867664852",
+//     appId: "1:304867664852:web:320d51439be9dcafba2c41"
+//   };
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+// export default app;
+
+// ==============================================
+
+
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAC9rdgoBx9Grbr4wZHYXhBUth749_M7jE",
+
+  authDomain: "pair-project-two-2796e.firebaseapp.com",
+
+  projectId: "pair-project-two-2796e",
+
+  storageBucket: "pair-project-two-2796e.appspot.com",
+
+  messagingSenderId: "671424892462",
+
+  appId: "1:671424892462:web:90ea73dd14183588cf19bf"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
