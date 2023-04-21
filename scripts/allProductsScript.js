6abffa5fb0b2a0df01f67a308fa02ecb7c5b9d96
dilpreetdhaliwal <dilpreet13dhaliwal@gmail.com>
2023-04-21 14:26:52 -0400

    april 21 huddle merge

---
+++ scripts/allProductsScript.js
@@ -0,0 +1,46 @@
+// initialize firebase
+import app from "./firebase.js";
+import {getDatabase, ref, onValue} from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
+
+// initialize database content
+const database = getDatabase(app)
+const dbRef = ref(database);
+
+// Next, we create a reference to point to our database:
+const inventoryRef = ref(database, '/inventory');
+
+const ulElement = document.querySelector("#productContainer");
+
+onValue(inventoryRef, (firebaseData) => {
+    const productData = firebaseData.val();
+    for (let key in productData) {
+        const imgUrl = productData[key].url;
+        const imgAlt = productData[key].title;
+        const productTitle = productData[key].title
+        const productPrice = productData[key].price
+        
+        const listItem = document.createElement("li");
+        listItem.id = key
+
+        const titleElement = document.createElement("h4")
+        titleElement.id = "searchId"
+        titleElement.innerText = productTitle
+
+        const imageElement = document.createElement("img")
+        imageElement.src = imgUrl
+        imageElement.alt = imgAlt
+
+        const priceElement = document.createElement("p")
+        priceElement.innerText = productPrice
+
+        listItem.append(titleElement, imageElement, priceElement);
+        ulElement.append(listItem);
+    }
+
+})
+
+// search bar
+
+const search = () => {
+    const searchBox = document.getElementById("searchBar").value.toLowerCase();
+}
