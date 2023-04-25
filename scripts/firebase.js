b0e5a94f0d1a71e4d078f4e3f015d9d657854ce6
noelle7 <noellehuang07@gmail.com>
2023-04-25 00:45:27 -0400

    finish Wishlist feature on product page

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -35,30 +35,3 @@ export default app;
 
 
 
-
-
-
-// ===============================
-
-
-//   // Import the functions you need from the SDKs you need
-//   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
-//   // TODO: Add SDKs for Firebase products that you want to use
-//   // https://firebase.google.com/docs/web/setup#available-libraries
-
-//   // Your web app's Firebase configuration
-//   const firebaseConfig = {
-//     apiKey: "AIzaSyCMf7c7lSpwaXADTvPUIU0xRW4SfaA8Zhk",
-//     authDomain: "pair-project-two.firebaseapp.com",
-//     databaseURL: "https://pair-project-two-default-rtdb.firebaseio.com",
-//     projectId: "pair-project-two",
-//     storageBucket: "pair-project-two.appspot.com",
-//     messagingSenderId: "304867664852",
-//     appId: "1:304867664852:web:320d51439be9dcafba2c41"
-//   };
-
-//   // Initialize Firebase
-//   const app = initializeApp(firebaseConfig);
-// export default app;
-
-
--- scripts/totalCartCount.js
+++
@@ -1,7 +0,0 @@
-
-
-// Create a new data structure
-// update the state (add, remove, filter)
-// make an array of objects (cart items)
-// use the length property on the array
-// array is adjusted and you only have to update the view of state
